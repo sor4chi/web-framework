fbdcd665ad8f4b0837a81f45de4a14b48090fcf8
sor4chi <sorakawa0218@gmail.com>
2023-05-02 03:45:44 +0900

    feat: create component class

---
+++ src/component.ts
@@ -0,0 +1,68 @@
+import { IVNode, VNode, Props } from "./v-node";
+
+type State = Record<string, any>;
+
+export class Component<S extends State = {}, P extends Props = {}> {
+  private _vNode: IVNode;
+  state: S;
+  props: P;
+  render: (
+    state: S,
+    props: P,
+    setState: (newState: Partial<S>) => void
+  ) => IVNode;
+  el?: HTMLElement;
+
+  constructor({
+    state,
+    props,
+    render,
+  }: {
+    state: S;
+    props: P;
+    render: (
+      state: S,
+      props: P,
+      setState: (newState: Partial<S>) => void
+    ) => IVNode;
+  }) {
+    this.state = state;
+    this.props = props;
+    this._vNode = render(state, props, this.setState.bind(this));
+    this.render = render;
+  }
+
+  private updateState(newState: Partial<S>) {
+    this.state = { ...this.state, ...newState };
+    this.rerender();
+  }
+
+  setState(newState: Partial<S>) {
+    this.updateState(newState);
+  }
+
+  rerender() {
+    const newVNode = this.render(
+      this.state,
+      this.props,
+      this.setState.bind(this)
+    );
+    this._vNode.update(newVNode);
+    const element = this._vNode.render();
+    if (this.el) {
+      this.el.replaceWith(element);
+    }
+    this.el = element;
+  }
+
+  mount(target: HTMLElement) {
+    const element = this._vNode.render();
+    target.appendChild(element);
+    this.el = element;
+  }
+
+  unmount(target: HTMLElement) {
+    const element = this._vNode.render();
+    target.removeChild(element);
+  }
+}
